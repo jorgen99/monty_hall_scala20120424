c654f320a76aebd450e7301e1b71cf3bd800f627
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2012-04-05 18:04:04 +0200

    Taking care of player name and starting new game displaying the doors.
    Configured the db-stuff in application.conf and the first db evolution.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,43 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import anorm._
+
+import views._
+import models._
 
 object Application extends Controller {
-  
+
+  val gameForm = Form(
+    mapping(
+      "id" -> ignored(NotAssigned:Pk[Long]),
+      "playerName" -> text
+    )(Game.apply)(Game.unapply)
+  )
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(html.index(gameForm))
+  }
+
+  def newGame = Action { implicit request =>
+    gameForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(html.index(formWithErrors)),
+      game => {
+        Game.insert(game) match {
+            case Some(id) => Redirect(routes.Application.game(id))
+            case None => Redirect(routes.Application.index)
+        }
+      }
+    )
+  }
+
+  def game(id: Long) = Action {
+    Game.findById(id).map { game =>
+      Ok(html.game(game))
+    }.getOrElse(NotFound)
   }
   
 }
